ccb14fb7de417a29a6ffeed7f7385d3fa4a354f2
govnoizzhopbl <gera.kibko.1997@gmail.com>
2023-06-15 20:11:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Learning_C_sharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {    
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Это изменение, которое надо замерджить");
+
+            Console.WriteLine("Это изменение, которое надо замерджить");
+
+            Console.ReadLine();
+        }
+    }
+}
